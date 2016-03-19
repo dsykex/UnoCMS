a44ec9d25840ae21478c8a9a3663a92b226660d4
dsykex <dezmensykes.ds@gmail.com>
2016-03-18 22:44:02 -0400

    More Doc work!

--- js/MyCtrl.js
+++ js/MyCtrl.js
@@ -1,6 +1,23 @@
 uno.controller('CustomCtrl', function($location, $timeout, $scope, $http, _uno, $rootScope){
     $scope.isStatic = Configs.isStatic;
     
+    var calcPos = function(){
+        //var elem = document.getElementById('header');
+        //elem.style.position = 'relative';
+        //elem.style.top = window.pageYOffset;
+    };
+    
+    window.addEventListener('scroll', calcPos);
+    
+    $scope.goToElement = function(id){
+        var elem =  document.getElementById(id);
+        var body = document.body.scrollHeight;
+        console.log(body);
+        
+        elem.scrollIntoView(true);
+        elem.style.opacity = 1;
+    }
+    
     $scope.getChildLength = function(id, db){
         return $scope.$parent[db].filter(function(value){ return (value.parent_id == id)});
     };
